98447997ab2d7fea3549328e3b5db9c524e76cee
hotvar <hanon813@gmail.com>
2021-06-01 13:38:38 +0900

    edit test code, add requirements.txt

--- SimpleOnlinePlot.py
+++ SimpleOnlinePlot.py
@@ -4,20 +4,20 @@ import time
 import requests
 
 class SOP():
-    def __init__(self, api_port=8000, app_port=8080, type='2d_line'):
+    def __init__(self, api_port=8000, app_port=8080, type='2d_line', chrome_path=''):
         self.api_port = api_port
         self.app_port = app_port
         self.api_url = f'http://127.0.0.1:{api_port}/api/meter/'
         self.type = type
+        self.chrome_path = chrome_path
         self.items = []
 
         self.p1 = subprocess.Popen(['python', './api/manage.py', 'runserver', f'{api_port}'], shell=True)
         self.p2 = subprocess.Popen(['cd', './app', '&', 'npm', 'run', 'serve', '--', '--port', f'{app_port}'], shell=True)
         time.sleep(5)
 
-        chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
         url = f'http://127.0.01:{app_port}'
-        webbrowser.register(chrome_path, None, webbrowser.BackgroundBrowser(chrome_path))
+        webbrowser.register(self.chrome_path, None, webbrowser.BackgroundBrowser(self.chrome_path))
         webbrowser.get(chrome_path).open(url)
 
     def meter(self, iter, value, periods=1):
--- test.py
+++ test.py
@@ -3,11 +3,14 @@ import time
 from SimpleOnlinePlot import SOP
 
 if __name__ == '__main__':
-    sop = SOP()
+    sop = SOP(api_port=8000,
+              app_port=8080,
+              type='2d_line',
+              chrome_path='C:/Program Files/Google/Chrome/Application/chrome.exe')
     iter = 0
     val = 1.0
     while True:
         iter += 1
         val *= 0.9
-        sop.meter(iter, val, periods=3)
+        sop.meter(iter, val, periods=1)
         time.sleep(3)
